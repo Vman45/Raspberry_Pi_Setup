7c3451dac23a6c29352c46bdbc36cfd796fa2ceb
Matthieu CHARETTE <matthieu.charette@gmail.com>
2019-05-04 07:39:16 +0200

    Add files via upload

--- Create_new_rasbian_image.sh
+++ Create_new_rasbian_image.sh
@@ -1,5 +1,4 @@
 mkdir /media/hdd
-rm /media/hdd/raspbian.zip
 curl --progress-bar -L -o /media/hdd/raspbian.zip https://downloads.raspberrypi.org/raspbian_lite_latest
 unzip -p /media/hdd/raspbian.zip | sudo dd of=/dev/sda bs=4M conv=fsync
 mkdir /media/cardreader
