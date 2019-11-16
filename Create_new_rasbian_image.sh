6d129d92127fcb81f976b16ac503cb30605b3a00
Matthieu CHARETTE <matthieu.charette@gmail.com>
2019-11-16 07:46:39 +0100

    Delete Create_new_rasbian_image.sh

--- Create_new_rasbian_image.sh
+++
@@ -1,7 +0,0 @@
-mkdir /media/hdd
-curl --progress-bar -L -o /media/hdd/raspbian.zip https://downloads.raspberrypi.org/raspbian_lite_latest
-unzip -p /media/hdd/raspbian.zip | sudo dd of=/dev/sda bs=4M conv=fsync
-mkdir /media/cardreader
-mount /dev/sda1 /media/cardreader
-touch /media/cardreader/ssh
-umount /media/cardreader
